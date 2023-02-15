3c8ab41dcad82723d9fac8620cfc51af8053574d
ALEJANDRO USME GALLEGO <alejandrousme235808@correo.itm.edu.co>
2023-02-15 18:22:57 -0500

    Create database SALES an create datacontext

---
+++ Sales.Api/Data/DateContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.Api.Data
+{
+    public class DateContext : DbContext
+    {
+        public DateContext(DbContextOptions<DateContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();   
+        }
+    }
+
+}
