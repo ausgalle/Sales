28728249a3fbac8bd50e439ee4fd15945dd41ce9
ALEJANDRO USME GALLEGO <alejandrousme235808@correo.itm.edu.co>
2023-02-19 21:50:35 -0500

    Begin controller conutry

---
+++ Sales.Api/Controllers/CountriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.Api.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DateContext _context;
+
+        public CountriesController(DateContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
