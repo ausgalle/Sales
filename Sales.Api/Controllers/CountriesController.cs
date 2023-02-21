8fe2fa76195fa5c1e03a8640b5fa70029be6b241
ALEJANDRO USME GALLEGO <alejandrousme235808@correo.itm.edu.co>
2023-02-20 19:08:09 -0500

    Solution error to create duplicate country and create function delete and edit

--- Sales.Api/Controllers/CountriesController.cs
+++ Sales.Api/Controllers/CountriesController.cs
@@ -38,16 +38,50 @@ namespace Sales.Api.Controllers
         public async Task<ActionResult> PostAsync(Country country)
         {
             _context.Add(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Ok(country);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un país con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
-		[HttpPut]
+
+        [HttpPut]
 		public async Task<ActionResult> PutAsync(Country country)
 		{
-			_context.Update(country);
-			await _context.SaveChangesAsync();
-			return Ok(country);
+			try
+			{
+                _context.Update(country);
+                await _context.SaveChangesAsync();
+                return Ok(country);
+            }
+			catch (DbUpdateException ex)
+			{
+				if (ex.InnerException!.Message.Contains("duplicate"))
+				{
+					return BadRequest("Ya existe un país con el mismo nombre.");
+				}
+                return BadRequest(ex.InnerException.Message);
+            }
+			catch(Exception ex)
+			{
+                return BadRequest(ex.Message);
+            }
 		}
 
 		[HttpDelete("{id:int}")]
